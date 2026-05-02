6c47de875512b7e2f482d1527cf358708fca7469
Yevheniia-Dzhos <yevheniia.dzhos@gmail.com>
2026-05-02 23:49:50 +0300

    HW1: add ESLint and fix lint errors

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,4 @@
-export function errorHandler(err, req, res, next) {
+export function errorHandler(err, req, res, _next) {
   const status = err.status || 500;
   const message = err.message || 'Internal Server Error';
   res.status(status).json({ error: message });
