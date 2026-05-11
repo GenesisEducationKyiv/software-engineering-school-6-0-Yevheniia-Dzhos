7495730eabc99738cf208601ec1918f1e3ae7246
Yevheniia-Dzhos <yevheniia.dzhos@gmail.com>
2026-05-11 20:09:12 +0300

    Refine ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,25 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, ...globals.node } }, rules: { 'no-unused-vars': ['error', { argsIgnorePattern: '^_' }] } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["dist/**", "node_modules/**"],
+    plugins: {
+      js,
+    },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ]);
